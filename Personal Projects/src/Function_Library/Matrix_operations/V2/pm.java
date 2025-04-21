43f217cd868b402b35e0c902c613524b242b6d6e
Ankur De <ankurde2005@gmail.com>
2025-04-21 22:24:44 +0530

    enhanced for loop

--- Personal Projects/src/Function_Library/Matrix_operations/V2/pm.java
+++ Personal Projects/src/Function_Library/Matrix_operations/V2/pm.java
@@ -3,7 +3,7 @@ package Function_Library.Matrix_operations.V2;
 public class pm {
     // extends err
     public static void printMat(double[][] matrix) {
-        for (int i = 0; i < matrix.length; i++) {
+        for (double[] doubles : matrix) {
             for (int j = 0; j < matrix[0].length; j++) {
                 System.out.print("  " + matrix[i][j]);
             }
