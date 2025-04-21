6e8088a1ebf676e1acaf1124c1f56b74992f4ad9
Ankur De <ankurde2005@gmail.com>
2025-04-21 22:23:58 +0530

    removed float

--- Personal Projects/src/Function_Library/Matrix_operations/V2/chkr.java
+++ Personal Projects/src/Function_Library/Matrix_operations/V2/chkr.java
@@ -11,14 +11,4 @@ public class chkr extends DP {
             throw new MatrixCorruptException("Either of the given matrix is corrupted");
         }
     }// Tested Ok
-
-    static void checker(float[][] A, float[][] B) {
-        if (A.length != B.length || A[0].length != B[0].length) {
-            throw new MatrixCorruptException("The operation number of rows or columns of matrix is not the same");
-        }
-
-        if (isCorrupt(A) || isCorrupt(B)) {
-            throw new MatrixCorruptException("Either of the given matrix is corrupted");
-        }
-    }// Tested Ok
 }
