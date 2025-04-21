6d511963f7c06c45336e4328fba5706ef32e1c70
Ankur De <ankurde2005@gmail.com>
2025-04-21 22:53:01 +0530

    typo

--- Personal Projects/src/Function_Library/Matrix_operations/V2/Subtraction.java
+++ Personal Projects/src/Function_Library/Matrix_operations/V2/Subtraction.java
@@ -6,7 +6,7 @@ import static Function_Library.Matrix_operations.V2.Checker.checker;
 final public class S {
     public static double[][] Subtr(double[][] A, double[][] B) {
         checker(A, B);
-        double[][] Subract = new double[A.length][A.length];
+        double[][] Subtract = new double[A.length][A.length];
         for (int i = 0; i < A.length; i++)
             for (int j = 0; j < A.length; j++)
                 Subtract[i][j] = (A[i][j] - B[i][j]);
