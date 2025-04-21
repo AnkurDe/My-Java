501c73f91e4404c232030d6a7c4fdc4f2d3e5fd8
Ankur De <ankurde2005@gmail.com>
2025-04-21 23:05:49 +0530

    handling all imports

--- Personal Projects/src/Function_Library/Matrix_operations/V2/Matrix.java
+++ Personal Projects/src/Function_Library/Matrix_operations/V2/Matrix.java
@@ -1,5 +1,28 @@
 package Function_Library.Matrix_operations.V2;
 
+
+import static Function_Library.Matrix_operations.V2.Addition.Add;
+import static Function_Library.Matrix_operations.V2.Checker.checker;
+import static Function_Library.Matrix_operations.V2.Comply.complm;
+import static Function_Library.Matrix_operations.V2.Comply.compla;
+import static Function_Library.Matrix_operations.V2.Determinant.*;
+import static Function_Library.Matrix_operations.V2.DiagonalProduct.*;
+import static Function_Library.Matrix_operations.V2.GaussianElimination.*;
+import static Function_Library.Matrix_operations.V2.Identity.*;
+import static Function_Library.Matrix_operations.V2.Inverse.*;
+import static Function_Library.Matrix_operations.V2.IsCorrupt.*;
+import static Function_Library.Matrix_operations.V2.IsSquare.*;
+import static Function_Library.Matrix_operations.V2.LUDecomposition.lu;
+import static Function_Library.Matrix_operations.V2.Multiplication.*;
+import static Function_Library.Matrix_operations.V2.PrintMatrix.printMat;
+import static Function_Library.Matrix_operations.V2.RREF.*;
+import static Function_Library.Matrix_operations.V2.Solve.*;
+import static Function_Library.Matrix_operations.V2.Subtraction.*;
+import static Function_Library.Matrix_operations.V2.SumOfElements.*;
+import static Function_Library.Matrix_operations.V2.SVD.*;
+import static Function_Library.Matrix_operations.V2.Transpose.*;
+import static Function_Library.Matrix_operations.V2.Zeromatr.Zeros;
+
 /**
  * This class serves as a placeholder for matrix operations.
  * It currently does not contain any methods or properties.
