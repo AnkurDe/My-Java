fc92b20de9f4605c2bfd92a6e198da12b0d42d51
Ankur De <ankurde2005@gmail.com>
2025-04-21 22:45:11 +0530

    class rename

--- Personal Projects/src/Function_Library/Matrix_operations/V2/DiagonalProduct.java
+++ Personal Projects/src/Function_Library/Matrix_operations/V2/DiagonalProduct.java
@@ -1,7 +1,7 @@
 package Function_Library.Matrix_operations.V2;
 
 // For taking diagonal products of elements
-final public class DP {
+final public class DiagonalProduct {
     public static double diag_prod(double[][] Matrix) {
         double dp = 1;
         for (int i = 0; i < Matrix.length; i++) {
