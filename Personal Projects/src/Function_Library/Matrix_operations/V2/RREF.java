5bad8fc9b33ff371ad3f62250144bf750b4834c6
Ankur De <ankurde2005@gmail.com>
2025-04-21 22:27:19 +0530

    removed float

--- Personal Projects/src/Function_Library/Matrix_operations/V2/RREF.java
+++ Personal Projects/src/Function_Library/Matrix_operations/V2/RREF.java
@@ -44,47 +44,4 @@ public class RREF extends isC {
         }
         return matrix;
     }
-
-    public static float[][] rref(float[][] matrix) {
-        int rows = matrix.length;
-        int cols = matrix[0].length;
-
-        int lead = 0;
-
-        for (int r = 0; r < rows; r++) {
-            if (lead >= cols) break;
-            int i = r;
-            while (matrix[i][lead] == 0) {
-                i++;
-                if (i == rows) {
-                    i = r;
-                    lead++;
-                    if (lead == cols) return matrix;
-                }
-            }
-
-            // Swap rows i and r
-            float[] temp = matrix[i];
-            matrix[i] = matrix[r];
-            matrix[r] = temp;
-
-            // Normalize the row by making the lead element 1
-            float leadValue = matrix[r][lead];
-            for (int j = 0; j < cols; j++) {
-                matrix[r][j] /= leadValue;
-            }
-
-            // Eliminate all other entries in the lead column
-            for (int k = 0; k < rows; k++) {
-                if (k != r) {
-                    float factor = matrix[k][lead];
-                    for (int j = 0; j < cols; j++) {
-                        matrix[k][j] -= factor * matrix[r][j];
-                    }
-                }
-            }
-            lead++;
-        }
-        return matrix;
-    }
 }
