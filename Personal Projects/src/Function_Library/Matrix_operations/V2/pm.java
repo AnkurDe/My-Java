832e94c246db23db32a0ae7b49c623ca266c4133
Ankur De <ankurde2005@gmail.com>
2025-04-21 22:24:13 +0530

    removed float

--- Personal Projects/src/Function_Library/Matrix_operations/V2/pm.java
+++ Personal Projects/src/Function_Library/Matrix_operations/V2/pm.java
@@ -10,13 +10,4 @@ public class pm {
             System.out.println("");
         }
     }
-
-    public static void printMat(float[][] matrix) {
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[0].length; j++) {
-                System.out.print("  " + matrix[i][j]);
-            }
-            System.out.println("");
-        }
-    }
 }
