a52bfc1ae513af6a9d90ff736c4b340068703913
Ankur De <ankurde2005@gmail.com>
2025-04-21 22:54:09 +0530

    method rename

--- Personal Projects/src/Function_Library/Matrix_operations/V2/TR.java
+++ Personal Projects/src/Function_Library/Matrix_operations/V2/TR.java
@@ -3,7 +3,7 @@ package Function_Library.Matrix_operations.V2;
 // Transpose of matrix
 // Incomplete
 final public class TR {
-    public static double[][] Transpose(double[][] input) {
+    public static double[][] transpose(double[][] input) {
         double[][] result = new double[input[0].length][input.length];
         for (int i = 0; i < input.length; i++)
             for (int j = 0; j < input[0].length; j++)
