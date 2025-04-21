c25cb40735ed3d536861244197e04eaa53976641
Ankur De <ankurde2005@gmail.com>
2025-04-21 22:32:12 +0530

    making final

--- Personal Projects/src/Function_Library/Matrix_operations/V2/DP.java
+++ Personal Projects/src/Function_Library/Matrix_operations/V2/DP.java
@@ -1,7 +1,7 @@
 package Function_Library.Matrix_operations.V2;
 
 // For taking diagonal products of elements
-public class DP extends RREF {
+final public class DP {
     public static double diag_prod(double[][] Matrix) {
         double dp = 1;
         for (int i = 0; i < Matrix.length; i++) {
