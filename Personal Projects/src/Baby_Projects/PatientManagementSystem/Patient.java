3b5a0023879d856227287c4c8337140451a7c179
Ankur De <ankurde2005@gmail.com>
2025-05-18 13:46:02 +0530

    better loops

--- Personal Projects/src/Baby_Projects/PatientManagementSystem/Patient.java
+++ Personal Projects/src/Baby_Projects/PatientManagementSystem/Patient.java
@@ -13,19 +13,10 @@ public class Patient {
             details.add(disease);
             return;
         }
-//        for (Disease d : details){
-//            double sl = Math.abs(d.sugar - disease.sugar);
-//            double temp = Math.abs(d.temp - disease.temp);
-//            if (sl<0.5 || temp < 0.5 || disease.name.equals(d.name) || disease.date.equals(d.date)){
-//                throw new DuplicateException();
-//            }
-//        }
-//        details.add(disease);
-
-        for (int i = 0; i < details.size(); i++){
-            double sl = Math.abs(details.get(i).getSugar() - disease.getSugar());
-            double temp = Math.abs(details.get(i).getTemp() - disease.getTemp());
-            if (sl<0.5 || temp < 0.5 || disease.getName().equals(details.get(i).getName()) || disease.getDate().equals(details.get(i).getDate())){
+        for (Disease d : details){
+            double sl = Math.abs(d.getSugar() - disease.getSugar());
+            double temp = Math.abs(d.getTemp() - disease.getTemp());
+            if (sl<0.5 || temp < 0.5 || disease.getName().equals(d.getName()) || disease.getDate().equals(d.getDate())){
                 throw new DuplicateException();
             }
         }
