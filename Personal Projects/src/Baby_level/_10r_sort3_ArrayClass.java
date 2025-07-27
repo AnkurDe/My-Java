bfa0826cec7c85ef07e2469e8770d89c5c58431a
Ankur De <ankurde2005@gmail.com>
2025-07-27 23:40:23 +0530

    Fixed

--- Personal Projects/src/Baby_level/_10r_sort3_ArrayClass.java
+++ Personal Projects/src/Baby_level/_10r_sort3_ArrayClass.java
@@ -5,52 +5,52 @@ package Baby_level;
 import java.util.*;
 import java.lang.*;
 
-// A class to represent a student.
-class student {
-    int rollno;
-    String name, address;
-
-    // Constructor
-    public student(int rollno, String name,
-                   String address)
-    {
-        this.rollno = rollno;
-        this.name = name;
-        this.address = address;
-    }
-
-    // Used to print student details in main()
-    public String toString()
-    {
-        return this.rollno + " "
-                + this.name + " "
-                + this.address;
-    }
-}
-
-class Sortbyroll_1 implements Comparator<student> {
-    // Used for sorting in ascending order of
-    // roll number
-    public int compare(student a, student b)
-    {
-        return a.rollno - b.rollno;
-    }
-}
-
 // Driver class
 class _10r_sort3_ArrayClass {
+    // A class to represent a student.
+    private static class Student {
+        int rollno;
+        String name, address;
+
+        // Constructor
+        public Student(int rollno, String name,
+                       String address)
+        {
+            this.rollno = rollno;
+            this.name = name;
+            this.address = address;
+        }
+
+        // Used to print student details in main()
+        public String toString()
+        {
+            return this.rollno + " "
+                    + this.name + " "
+                    + this.address;
+        }
+    }
+
+    private static class Sortbyroll_1 implements Comparator<Student> {
+        // Used for sorting in ascending order of
+        // roll number
+        public int compare(Student a, Student b)
+        {
+            return a.rollno - b.rollno;
+        }
+    }
+
     public static void main(String[] args)
     {
-        student[] arr = { new student(111, "bbbb", "london"),
-                new student(131, "aaaa", "nyc"),
-                new student(121, "cccc", "jaipur") };
+        Student[] arr = { new Student(111, "bbbb", "london"),
+                new Student(131, "aaaa", "nyc"),
+                new Student(121, "cccc", "jaipur") };
 
         System.out.println("Unsorted");
-        for (student value : arr) System.out.println(value);
+        for (Student value : arr) System.out.println(value);
 
         Arrays.sort(arr, 1, 2, new Sortbyroll_1());
 
         System.out.println("\nSorted by rollno");
-        for (Baby_level.student student : arr) System.out.println(student);
+        for (Student student : arr) System.out.println(student);
     }
 }
